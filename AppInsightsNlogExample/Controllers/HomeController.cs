61856eefd838a8940019b8d67ee4758d6135eccf
Mike Breault <mikeb@mcrtechnologies.com>
2019-11-21 11:41:30 -0500

    init commit

---
+++ AppInsightsNlogExample/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using AppInsightsNlogExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using System.Diagnostics;
+
+namespace AppInsightsNlogExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly Logger logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+        public IActionResult Index()
+        {
+            logger.Trace("HomeControler.Index");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            logger.Trace("HomeControler.Privacy");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
